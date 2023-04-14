578575c158082e2e15b5923ae99d1aea4a32e309
Marta Sá <martasa57@hotmail.com>
2023-04-14 20:12:55 +0100

    Fase 2 feita

---
+++ Trabalho/Engine/code/color.h
@@ -0,0 +1,20 @@
+#ifndef Color_h
+#define Color_h
+
+class Color {
+
+private:
+	float x;
+	float y;
+	float z;
+
+public:
+	Color() {
+		this->x = 0;
+		this->y = 0;
+		this->z = 0;
+	}
+
+
+
+#endif
